1bc4b7999ecab9708728b909a90b5e1c60e05e16
David-VTUK <david.holder@gmail.com>
2023-04-06 13:42:18 +0100

    Add test only CI job and modify script to inject a project

--- .github/scripts/register-downstream-clusters.sh
+++ .github/scripts/register-downstream-clusters.sh
@@ -39,11 +39,32 @@ userPrincipal:
   provider: local
 EOF
 
+kubectl apply -f - <<EOF
+apiVersion: management.cattle.io/v3
+kind: Project
+metadata:
+  labels:
+    cattle.io/creator: norman
+  namespace: local
+  name: ci-project
+spec:
+  clusterName: local
+  displayName: ci-project
+  namespaceDefaultResourceQuota:
+    limit:
+      configMaps: '10'
+  resourceQuota:
+    limit:
+      configMaps: '1000'
+    usedLimit: {}
+EOF
+
 echo -e "4\n" | rancher login "https://$url" --token "$token" --skip-verify
 
 rancher clusters create second --import
 
 kubectl config use-context "$cluster_downstream"
+
 rancher cluster import second
 rancher cluster import second | grep curl | sh
 
